29655627fe62af48cece99a24e979c5dbedc49d5
Zubin <me@zubin.org.in>
2023-05-05 23:35:08 +0530

    Init

---
+++ README.md
@@ -0,0 +1,50 @@
+# Neeva Telegram Bot
+
+Use Neeva AI chat bot in Telegram.
+
+<br>
+
+### Install
+
+1. Clone repo.
+2. Run ```pip3 install -r requirements.txt``` in project folder.
+3. Rename .env.example to .env and provide tokens.
+4. Run ```python3 bot.py``` to start the bot.
+
+[Instructions for Poe token](https://github.com/ading2210/poe-api#finding-your-token)
+
+<details>
+
+<summary>
+Bot admin ID
+</summary>
+
+<br>
+
+BOT_ADMIN is needed to prevent unauthorized access.
+
+</details>
+
+<br>
+
+#### It's advisable to run the bot using PM2 or any startup manager for persistent execution.
+
+<br>
+
+### Uninstall
+
+1. Use ```rm -rf```.
+
+*Note:* If you're unfamiliar with this command, delete project folder from file explorer.
+
+<br>
+
+### Mechanism
+
+This uses the [poe-api](https://github.com/ading2210/poe-api) module to interact with Neeva AI using Poe.
+
+<br>
+
+### License
+
+AGPL-3.0 ©️ Zubin
---
+++ bot.py
@@ -0,0 +1,41 @@
+import poe
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("TOKEN")
+bot_token = os.getenv("BOT")
+adminId = int(os.getenv("BOT_ADMIN"))
+
+bot = telebot.TeleBot(bot_token)
+client = poe.Client(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(
+        message, "<b>Welcome!</b> ✨\n<i>Send any query or ask questions.</i>", parse_mode="HTML")
+
+
+@bot.message_handler(func=lambda message: True)
+def claude(message):
+    if message.from_user.id != adminId:
+        bot.reply_to(
+            message, "<b>You are not authorized to use this bot.</b>", parse_mode="HTML")
+        return
+    try:
+        bot.send_chat_action(message.chat.id, 'typing')
+        for chunk in client.send_message(
+            chatbot="hutia",
+            message=message.text,
+            with_chat_break="yes",
+            timeout=20,
+        ):
+            pass
+        bot.reply_to(message, chunk["text"])
+    except Exception as e:
+        bot.reply_to(message, f"Oops! Something went wrong. {e}")
+
+
+bot.polling()
